e377cf0bd3d235a55bd3fdeed36feee961d304bb
Фомин Дмитрий - 051 группа <dfom@1c.ru>
2022-12-14 20:32:08 +0300

    preDev Commit

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,38 @@
-import MainContainer from "../components/MainContainer";
-import { parseCookies } from "../helpersJs/index";
+import MainContainer from '../components/MainContainer';
+import { parseCookies } from '../helpersJs/index';
 
 const Index = ({ data }) => {
-  return <MainContainer email={data.email}></MainContainer>;
+    return <MainContainer email={data.email}></MainContainer>;
 };
 
 export async function getServerSideProps({ query, req }) {
-  const { number, filter, id } = query;
-  const cookie = parseCookies(req);
-  const cookieData =
-    "userData" in cookie ? await JSON.parse(cookie.userData) : false;
-  if (cookieData) {
-    if (id) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: `/content/${id}?number=${number}&filter=${filter}`,
-        },
-      };
+    //main branch
+    const { number, filter, id } = query;
+    const cookie = parseCookies(req);
+    const cookieData =
+        'userData' in cookie ? await JSON.parse(cookie.userData) : false;
+    if (cookieData) {
+        if (id) {
+            return {
+                redirect: {
+                    permanent: false,
+                    destination: `/content/${id}?number=${number}&filter=${filter}`,
+                },
+            };
+        } else {
+            return {
+                redirect: {
+                    permanent: false,
+                    destination: `/content/0?number=10&filter=start`,
+                },
+            };
+        }
     } else {
-      return {
-        redirect: {
-          permanent: false,
-          destination: `/content/0?number=10&filter=start`,
-        },
-      };
+        return {
+            props: {
+                data: { email: false },
+            },
+        };
     }
-  } else {
-    return {
-      props: {
-        data: { email: false },
-      },
-    };
-  }
 }
 export default Index;
